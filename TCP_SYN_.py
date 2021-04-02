9119accc148a31e2653ad3d3e5e732e3ea8105cd
Tasos Torounidis <tasostor91@gmail.com>
2021-04-02 22:39:02 +0100

    added closed and filtered ports, fixed the arp scan

--- TCP_SYN_.py
+++ TCP_SYN_.py
@@ -16,7 +16,7 @@ import ipaddress
 
 def arp_scan(target):
     """
-    target_range: the targer host(s) to perform the TCP_SYN scan. type list of 1 or more ip addresses
+    target: the targer host(s) to perform the TCP_SYN scan. type string
     """
     #Dictionary to save all the host results including: IP address, Open Ports, Mac Address and Latency
     results = {}
@@ -26,46 +26,30 @@ def arp_scan(target):
         "192.168.0.1":{
             "MAC" = "xxx",
             "latency" = "xxxx",
-            "ports" = {
-                12: "Open",
-                23: "Filtered"
-                34: "Closed"
-            },
-        },
+            "open_ports" = [1,2,3,4],
+            "filtered_ports"= [56]
+        }
     }
 
     """
-    # Send ARP request to the hosts to see of they are alive. This is done to speed the port scanning proccess
+    # Broadcast ARP request to the network to find live hosts.
     # Verbose of 0 to supress screen logging
     # Stuck the Layer 2 and Layer 3 packets
-    
     arp_packet = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=target)
     
-    # Send the packets using the scapy srp() method. 
-    ans,_ = srp(arp_packet,timeout=2, retry=2, verbose=0)
+    # Send the packets using the scapy srp() method.
+    ans,_ = srp(arp_packet,timeout=10,verbose=0)
     
-    # Loop through the answers and update the results dictionary
+    # Enumerate all asnwers and append to result dictionary as required
     for answer in ans:
         results[answer[1].psrc] = {
             "MAC Address":answer[1].hwsrc,
             "latency": (answer[0][0].sent_time - answer[0][1].time)*1000, # convert to ms
-            "ports": {},
+            "ports": []
             }
-
-    #Add the current host to the list of live hosts for TCP scan.
-    self_hostname = socket.gethostname()
-    self_ip= socket.gethostbyname(self_hostname)
     
-    if target != self_ip:
-        results[self_ip] = {
-            "MAC Address":None,
-                "latency": None,
-                "ports": {},
-        }
-
     return results
 
- 
 def TCP_SYN_port_scan(dict, ports):
     """
     dict: a dictionary generated from the function arp_scan. type dictionary
@@ -78,21 +62,13 @@ def TCP_SYN_port_scan(dict, ports):
         # Stuck the Layer 3 header and the TCP payload
         tcp_package = IP(dst=host) / TCP(dport=ports, flags="S")
         #send the TCP package  for  all ports and collect all answers
-        answered, unanswered = sr(tcp_package, timeout=2, verbose= False)
-        
+        ans,_ = sr(tcp_package, timeout=2, verbose= False)
         # for each of the answers received,
-        for answer in answered:
-            # if the answer is "SYN AKN" or 0x12 in hex, append the port to the results dictonary as open
+        for answer in ans:
+            # if the answer is "SYN AKN" or 0x12 in hex, append the port to the results dictonary
             if answer[1][TCP].flags == 0x12:
-                hosts[host]["ports"][answer[1][TCP].sport] = "open"
-                send(IP(dst=host) / TCP(dport=answer[1][TCP].sport, flags="R"), verbose= False)
-            # if the answer is "RESET" append the port to the results dictonary as closed
-            elif answer[1][TCP].flags == 0x14:
-                hosts[host]["ports"][answer[1][TCP].sport] = "closed"
-        # for each of the unanswersed ports received, add them to the results as filtered
-        for not_answer in unanswered:
-            hosts[host]["ports"][not_answer[1][TCP].dport] = "filtered"
-               
+                hosts[host]["open_ports"].append(answer[1][TCP].sport)
+                
     return hosts
 
 def print_save_report(results):
@@ -111,51 +87,35 @@ def print_save_report(results):
     #sort host IPs smaller to bigger:
     hosts.sort()
 
-    #last, add the to the print list the ip of the host that performed the scan
+    #last add the to the print list the ip of the host that performed the scan
     if self_ip in results:
         hosts.append(self_ip)
-
+        
     # Enumerate all hosts and print the report
     for host in hosts:
-        # extract the scanned ports and sort
-        ports = sorted(results[host]["ports"].items())
-        len_open = len(filter(lambda s: s[1]=="open" ,ports))
-        len_closed = len(filter(lambda s: s[1]=="closed" ,ports))
-        len_filtered = len(filter(lambda s: s[1]=="filtered" ,ports))
-
         print("TCP_SYN Scan report for {}".format(host))
         print("TCP_SYN Scan report for {}".format(host), file=savefile)
-        if results[host]["latency"] is not None:
-            print("Host is up ({}ms latency).".format(results[host]["latency"]))
-            print("Host is up ({}ms latency).".format(results[host]["latency"]), file= savefile)
-        
-        if len_open == 0 and len_closed == 0:
-            print("All {} scanned ports on {} are most likely filtered".format(len(ports),host))
+        print("Host is up ({}ms latency).".format(results[host]["latency"]))
+        print("Host is up ({}ms latency).".format(results[host]["latency"]), file= savefile)
+        if len(results[host]["open_ports"]) == 0:
+            print("All specified for scan ports on {} are most likely filtered or closed".format(host))
             print(
-                "All {} scanned ports on {} are most likely filtered"\
-                    .format(len(ports),host), file= savefile)
-        elif len_open == 0 and len_filtered == 0:
-            print("All {} scanned ports on {} are closed".format(len(ports),host))
-            print("All {} scanned ports on {} are closed".format(len(ports),host), file= savefile)
+                "All specified for scan ports on {} are most likely filtered or closed"\
+                    .format(host), file= savefile)
         else:
-            print("Not shown: {} filtered ports and {} closed ports".format(len_filtered,len_closed))
-            print("Not shown: {} filtered ports and {} closed ports".format(len_filtered,len_closed), file= savefile)
-        if len_open !=0:    
-            print("{:<12}{:<8}{:<20}".format("PORT","STATE","SERVICE"))
-            print("{:<12}{:<8}{:<20}".format("PORT","STATE","SERVICE"), file= savefile)
-            for port in filter(lambda s: s[1]=="open" ,ports):
-                try:
-                    print("{:<12}{:<8}{:<20}".format(str(port[0])+"/tcp","open", socket.getservbyport(int(port[0]))))
-                    print("{:<12}{:<8}{:<20}".format(str(port[0])+"/tcp","open", socket.getservbyport(int(port[0]))), file= savefile)
-                except socket.error:
-                    print("{:<12}{:<8}{:<20}".format(str(port[0])+"/tcp","open","unknown"))
-                    print("{:<12}{:<8}{:<20}".format(str(port[0])+"/tcp","open","unknown"), file=savefile)
-        
-        if results[host]["MAC Address"] is not None:
-            print("MAC Address: {}".format(results[host]["MAC Address"]))
-            print("MAC Address: {}".format(results[host]["MAC Address"]), file= savefile)
-            print("")
-            print("", file=savefile)
+            print("PORT\t  STATE\t  SERVICE")
+            print("PORT\t  STATE\t  SERVICE", file= savefile)
+            try:
+                for port in results[host]["open_ports"]:
+                    print("{}/tcp\t  open\t  {}".format(port, socket.getservbyport(int(port))))
+                    print("{}/tcp\t  open\t  {}".format(port, socket.getservbyport(int(port))), file= savefile)
+            except socket.error:
+                    print("{}/tcp\t  open\t  uknown".format(port))
+                    print("{}/tcp\t  open\t  uknown".format(port), file=savefile)
+        print("MAC Address: {}".format(results[host]["MAC Address"]))
+        print("MAC Address: {}".format(results[host]["MAC Address"]), file= savefile)
+        print("")
+        print("", file=savefile)
     
 
 if __name__ == "__main__" :
@@ -177,7 +137,7 @@ if __name__ == "__main__" :
 
     args = parser.parse_args()
 
-    # Check that at maximum 1 port argument is provided. 
+    #Check that at maximum 1 port argument is provided. 
     if (args.port or args.portrange or args.portlist) is None:
         print("Error! Port not provided. See --help for details.\nExiting..")
         sys.exit(1)
@@ -194,6 +154,8 @@ if __name__ == "__main__" :
     else: 
         port = args.portlist
 
+    # Get the target from the arguments on which to perform the TCP_SYN Scan
+    target = args.target
     # If specified, get the filename argument which will be used to save the results,
     #   otherwise use the default "TCP_SYN report + current time"
     
@@ -203,7 +165,6 @@ if __name__ == "__main__" :
         now = dt.now()
         time_filename = now.strftime("%Y-%m-%d-%H-%M-%S")
         filename = str("TCP_SYN_"+time_filename+".txt") 
-    
     savefile = open(filename, "a")
 
     ## All statements are printed to both the screen and to a file.
@@ -211,14 +172,14 @@ if __name__ == "__main__" :
     print("Starting TCP_SYN Scan at {}.\nTarget: {}\nPort(s): {}\nResults will be saved under \"{}\" "\
         .format(
             time_now.strftime("%Y-%m-%d %H:%M:%S"),
-            args.target,
+            target,
             port,
             filename
             ))
     print("Starting TCP_SYN Scan at {}.\nTarget: {}\nPort(s): {}\nResults will be saved under \"{}\" "\
         .format(
             time_now.strftime("%Y-%m-%d %H:%M:%S"),
-            args.target,
+            target,
             port,
             filename
             ), file=savefile)
@@ -230,7 +191,7 @@ if __name__ == "__main__" :
     print("Scanning the network for live host(s)...", file =savefile)
     
     #Step 1) Find live hosts in the network.  
-    live_hosts = arp_scan(args.target)
+    live_hosts = arp_scan(target)
     print("Found {} live hosts".format(len(live_hosts)))
     print("Found {} live hosts".format(len(live_hosts)), file= savefile)
     
@@ -238,7 +199,6 @@ if __name__ == "__main__" :
     print("Scanning for open TCP ports on live hosts...", file= savefile)
     #Step 2) Scan for open TCP ports on the live hosts
     tcp_scan = TCP_SYN_port_scan(live_hosts, port)
-    
 
     #Step 3) Print and save the report
     print_save_report(tcp_scan)
@@ -251,4 +211,5 @@ if __name__ == "__main__" :
 
     # Save and close the log file
     savefile.close()
+    
     
